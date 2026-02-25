4a3a5dcccb4106390c92ed29d0fd12f25120289e
vincent <vinayakgeorgeciobanu@gmail.com>
2026-02-25 17:21:13 +0100

    implement dynamic stack

---
+++ src/stack/DynamicStack.java
@@ -0,0 +1,65 @@
+package stack;
+
+public class DynamicStack<T> {
+
+    private Object[] elements;
+    private int size;
+    private int capacity;
+
+    public DynamicStack() {
+        this.elements = new Object[capacity];
+        this.size = 0;
+        this.capacity = 1;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == capacity;
+    }
+
+    public T peek() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        //noinspection unchecked
+        return (T) elements[size - 1];
+    }
+
+    public T pop() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        size--;
+        //noinspection unchecked
+        T res = (T) elements[size];
+        elements[size] = null;
+
+        if (capacity > 1 && size < capacity * 0.25) {
+            Object[] newElements = new Object[capacity / 2];
+            for (int i = 0; i < size; i++) {
+                newElements[i] = elements[i];
+            }
+            elements = newElements;
+        }
+        return res;
+    }
+
+    public void push(T element) {
+        if (isFull()) {
+            Object[] newElements = new Object[capacity * 2];
+            for (int i = 0; i < size; i++) {
+                newElements[i] = elements[i];
+            }
+            elements = newElements;
+        }
+        elements[size] = element;
+        size++;
+    }
+}
