72f007c9bc1eb494c4db56d99cef124cf63bfa72
vincent <vinayakgeorgeciobanu@gmail.com>
2026-02-26 13:21:09 +0100

    implement simple queue with linked list

---
+++ src/queue/Queue.java
@@ -0,0 +1,27 @@
+package queue;
+
+import java.util.LinkedList;
+
+public class Queue<T> {
+
+    private LinkedList<T> list = new LinkedList<>();
+
+    public Queue() {
+        this.list = new LinkedList<>();
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public void enqueue(T t) {
+        list.addLast(t);
+    }
+
+    public T dequeue() {
+        if (list.isEmpty()) {
+            throw new EmptyQueueException();
+        }
+        return list.removeFirst();
+    }
+}
