c9960edb82816e0549eb3a74528edcb66472ae8c
vincent <vinayakgeorgeciobanu@gmail.com>
2026-02-26 13:21:26 +0100

    implement queue with two stacks

---
+++ src/queue/StackQueue.java
@@ -0,0 +1,54 @@
+package queue;
+
+import stack.DynamicStack;
+
+import java.util.NoSuchElementException;
+
+public class StackQueue<T> {
+
+    private DynamicStack<T> stackIn;
+
+    private DynamicStack<T> stackOut;
+
+
+    public StackQueue() {
+        stackIn = new DynamicStack<>();
+        stackOut = new DynamicStack<>();
+    }
+
+    public boolean isEmpty() {
+        return stackIn.isEmpty() && stackOut.isEmpty();
+    }
+
+    public int size() {
+        return stackIn.size() + stackOut.size();
+    }
+
+    public void enqueue(T element) {
+        stackIn.push(element);
+    }
+
+    private void shiftStacks() {
+        if (stackOut.isEmpty()) {
+            while (!stackIn.isEmpty()) {
+                stackOut.push(stackIn.pop());
+            }
+        }
+    }
+
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new EmptyQueueException();
+        }
+        shiftStacks();
+        return stackOut.pop();
+    }
+
+    public T first() {
+        if (isEmpty()) {
+            throw new EmptyQueueException();
+        }
+        shiftStacks();
+        return stackOut.peek();
+    }
+}
