35068690395e00f8c9c792eb254811d7b6f7cf59
vincent <vinayakgeorgeciobanu@gmail.com>
2026-02-28 14:19:04 +0100

    implemented tree sum

--- src/tree/TreeUtils.java
+++ src/tree/TreeUtils.java
@@ -5,6 +5,7 @@ import queue.Queue;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class TreeUtils {
 
@@ -24,6 +25,13 @@ public class TreeUtils {
         return new Tree<T>(t.getObject(), copyChildren);
     }
 
+    /**
+     * Computes whether the BinaryTree is complete.
+     *
+     * @param root
+     *     the SimpleBinaryTree to check.
+     * @return true iff the BinaryTree is complete, else false.
+     */
     public static boolean isBinaryTreeComplete(SimpleBinaryTree root) throws EmptyQueueException {
         if (root == null) {
             return true;
@@ -48,5 +56,26 @@ public class TreeUtils {
             }
         }
         return true;
+    }
+
+    /**
+     * Sums the values of the nodes of two n-ary trees.
+     * @param t1 - first tree to sum values for
+     * @param t2 - second tree to sum values for
+     * @return a new tree in which every node contains the sum of the values of the nodes at the corresponding positions in t1 and t2
+     */
+    public static Tree<Integer> sum(Tree<Integer> t1, Tree<Integer> t2) {
+        if (t1 == null && t2 == null) {
+            return null;
+        }
+        List<Tree<Integer>> sumChildren = new ArrayList<>();
+        List<Tree<Integer>> c1 = t1.getChildren();
+        List<Tree<Integer>> c2 = t2.getChildren();
+
+        for (int i = 0; i < c1.size(); i++) {
+            sumChildren.add(sum(c1.get(i), c2.get(i)));
+        }
+        return new Tree<>(t1.getObject() + t2.getObject(), sumChildren);
+
     }
 }
