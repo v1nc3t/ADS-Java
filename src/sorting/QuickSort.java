5b38fbb273b3c0a416b929aba90e4431f9276b63
vincent <vinayakgeorgeciobanu@gmail.com>
2026-03-03 19:29:44 +0100

    implement quick sort

---
+++ src/sorting/QuickSort.java
@@ -0,0 +1,32 @@
+package sorting;
+
+public final class QuickSort {
+
+    public static void quickSort(int[] arr, int l, int r) {
+        if  (l < r) {
+            int pivot = partition(arr, l, r);
+
+            quickSort(arr, l, pivot - 1);
+            quickSort(arr, pivot + 1, r);
+        }
+    }
+
+    private static int partition(int[] arr, int l, int r) {
+        int pivot = arr[r];
+        int i = l - 1;
+        for (int j = l; j < r; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, r);
+        return i + 1;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
