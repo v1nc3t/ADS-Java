462451a3fec0551af8be71654d4d1c7e4ba544d3
vincent <vinayakgeorgeciobanu@gmail.com>
2026-03-03 19:38:42 +0100

    implement radix sort

---
+++ src/sorting/RadixSort.java
@@ -0,0 +1,45 @@
+package sorting;
+
+public final class RadixSort {
+
+    public static void radixSort(int[] arr) {
+        int n = arr.length;
+        int m = getMax(arr);
+        for (int exp = 1; m / exp > 0; exp *= 10) {
+            countSort(arr, n, exp);
+        }
+    }
+
+    private static void countSort(int[] arr, int n, int exp) {
+        int[] output = new int[n];
+        int[] count = new int[10];
+
+        for (int i = 0; i < n; i++) {
+            count[(arr[i] / exp) % 10]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = output[i];
+        }
+    }
+
+
+    private static int getMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
