79e6c45d89f7df877a40c37ac5775a53b1824fb1
vincent <vinayakgeorgeciobanu@gmail.com>
2026-03-07 13:26:31 +0100

    complete quick sort

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -1,6 +1,34 @@
 package sorting;
 
+import java.util.Random;
+
 public final class QuickSort {
+
+    public static void quickSortInPlace(int[] arr, int l, int r) {
+        if (l >= r) return;
+        // randomize
+        // random(arr, l, r);
+
+        // choose last element as pivot
+        int pivot = arr[r];
+        int left = l;
+        int right = r - 1;
+
+        while (left <= right) {
+            while (left <= right && arr[left] < pivot) left++;
+            while (left <= right && arr[right] > pivot) right--;
+
+            if (left <= right) {
+                swap(arr, left, right);
+                left++;
+                right--;
+            }
+        }
+        swap(arr, left, r);
+
+        quickSortInPlace(arr, l, left - 1);
+        quickSortInPlace(arr, left + 1, r);
+    }
 
     public static void quickSort(int[] arr, int l, int r) {
         if  (l < r) {
@@ -12,6 +40,8 @@ public final class QuickSort {
     }
 
     private static int partition(int[] arr, int l, int r) {
+        // randomize
+        // random(arr, l, r);
         int pivot = arr[r];
         int i = l - 1;
         for (int j = l; j < r; j++) {
@@ -28,5 +58,12 @@ public final class QuickSort {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+    }
+
+    private static void random(int[] arr, int l, int r) {
+        Random random = new Random();
+        int pivot = random.nextInt(r - l + 1) + l;
+
+        swap(arr, pivot, r);
     }
 }
