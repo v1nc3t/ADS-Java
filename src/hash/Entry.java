b98061df269283c0ebdf85d68796a0b3afc28f28
vincent <vinayakgeorgeciobanu@gmail.com>
2026-03-18 10:29:22 +0100

    implemented chained hash table

---
+++ src/hash/Entry.java
@@ -0,0 +1,30 @@
+package hash;
+
+public class Entry {
+
+    public final String key;
+
+    public final Integer value;
+
+    public Entry (String key, Integer value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public boolean equals(String s) {
+        return s == null && key == null || key.equals(s);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return this == o || o != null && getClass() == o.getClass() && this.equals(((Entry) o).key);
+    }
+}
